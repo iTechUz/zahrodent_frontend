0e1b22e900391370d1c6374f9406fd200ac4a4ac
shohjahon-asqarov <shohjahonasqarov.web@gmail.com>
2026-04-24 16:02:56 +0500

    chore: update dev server host and configure allowed hosts for origin validation

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,9 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
   server: {
-    host: "::",
+    host: "0.0.0.0",
     port: 8080,
+    allowedHosts: ["admin.zahro.iqroagency.uz"],
     hmr: {
       overlay: false,
     },
